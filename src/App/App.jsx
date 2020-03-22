3ef5cda55accfab0c76755a9e7087297923d753f
Alex H <cvcd005@gmail.com>
2020-03-22 10:10:13 +0300

    test pret

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
-  return <div prop={1} prop2={2} prop3={3} prop43={32} proppp={123123}>This is Pokedex</div>;
+  return <div prop={1} prop2={2} prop3={3} prop43={32} proppp={123123} adsfasdfas={123}>This is Pokedex</div>;
 };
 
 export default App;
