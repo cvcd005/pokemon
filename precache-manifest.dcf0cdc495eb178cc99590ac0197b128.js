79261afeec5f8fc3137b05a8432d1abea826cbd3
Alex H <cvcd005@gmail.com>
2020-03-23 10:04:57 +0300

    Updates

---
+++ precache-manifest.dcf0cdc495eb178cc99590ac0197b128.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f397bfc842cf87bd71af570e4b54c500",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "5c67064dbb3e3d288227",
+    "url": "/pokemon/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "2301a297db06172af2f4",
+    "url": "/pokemon/static/css/main.aa6e089b.chunk.css"
+  },
+  {
+    "revision": "5c67064dbb3e3d288227",
+    "url": "/pokemon/static/js/2.a1f4c1d9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/pokemon/static/js/2.a1f4c1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2301a297db06172af2f4",
+    "url": "/pokemon/static/js/main.b63c782c.chunk.js"
+  },
+  {
+    "revision": "e1366ef5cfb767bc3a5a",
+    "url": "/pokemon/static/js/runtime-main.2bfe477e.js"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/pokemon/static/media/opensans.811c8bae.woff"
+  },
+  {
+    "revision": "6f05808b4f6a11d3feb1716291c7c038",
+    "url": "/pokemon/static/media/opensansbold.6f05808b.woff"
+  }
+]);
